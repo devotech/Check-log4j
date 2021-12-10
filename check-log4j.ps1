a8fcacd76b71dabf809373c4900b1a89c0155b97
devotech <57639218+devotech@users.noreply.github.com>
2021-12-10 22:30:39 +0100

    log4shell ps1-check
    make sure to replace $server and $path as those are placeholders.
    
    I recommend splitting up the scan, as this will take a LONG time.

---
+++ check-log4j.ps1
@@ -0,0 +1,29 @@
+$prefix = Read-host "Enter used prefix for all servers"
+$computerNames = @(get-adcomputer -Filter { OperatingSystem -Like '*Windows Server*' -and name -like "$prefix*" } )
+$ignoreDrives = @("A", "B" ) # A and B not relevant, D is temp drive of Azure VMs
+$keyword = "*log4j-*.jar"
+$logfile = "\\$server\$path\log4j-servercheck.log"
+
+Start-Transcript -Path $logfile -NoClobber
+
+foreach ($computer in $computerNames) {
+    $computer.name # Show computername
+    if ((Test-Connection -computername $computer.name -Quiet) -eq $true) {
+        Invoke-Command -ComputerName $computer.name -ScriptBlock {
+            $drives = Get-PSDrive -PSProvider FileSystem
+            foreach ($drive in $drives) {
+                if ($drive.Name -notin $using:ignoreDrives) {
+                    $items = Get-ChildItem -Path $drive.Root -Filter $using:keyword -ErrorAction SilentlyContinue -File -Recurse
+                    foreach ($item in $items) {
+                        $item.FullName # Show all files found with full drive and path
+                    }
+                }
+            }
+        }
+    }
+    else{
+     "Offline"
+     }
+}
+
+Stop-Transcript
